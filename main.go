06c35d0133eb63b22da0549bf27fbb1a2400950b
Hoon <kimhoon0316@gmail.com>
2020-11-03 16:17:14 +0900

    SelectionSort

--- main.go
+++ main.go
@@ -40,10 +40,34 @@ func InsertionSort(n []int) []int {
 	return n
 }
 
+// SelectionSort !
+func SelectionSort(n []int) []int {
+	for i := 1; i < len(n)-1; i++ {
+		j := i + 1
+		minIndex := i
+
+		if j < len(n) {
+			if n[j] < n[minIndex] {
+				minIndex = j
+			}
+			j++
+		}
+
+		if minIndex != i {
+			temp := n[i]
+			n[i] = n[minIndex]
+			n[minIndex] = temp
+		}
+	}
+
+	return n
+}
+
 func main() {
 	n := []int{1, 39, 2, 9, 7, 54, 11}
 	fmt.Println("BubbleSort:", BubbleSort(n))
 
 	fmt.Println("RecursiveBubbleSort:", RecursiveBubbleSort(n, len(n)))
 	fmt.Println("InsertionSort", InsertionSort(n))
+	fmt.Println("SelectionSort", SelectionSort(n))
 }
