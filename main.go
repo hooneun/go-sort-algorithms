25bf65fb047e737d23a841348130b6ec8b0d9d46
Hoon <kimhoon0316@gmail.com>
2020-11-05 14:55:40 +0900

    merge sort add !

--- main.go
+++ main.go
@@ -63,6 +63,57 @@ func SelectionSort(n []int) []int {
 	return n
 }
 
+// Merge !
+func Merge(fp []int, sp []int) []int {
+	var n = make([]int, len(fp)+len(sp))
+
+	var fpIndex = 0
+	var spIndex = 0
+
+	var nIndex = 0
+
+	for fpIndex < len(fp) && spIndex < len(sp) {
+		if fp[fpIndex] < sp[spIndex] {
+			n[nIndex] = fp[fpIndex]
+			fpIndex++
+		} else if sp[spIndex] < fp[fpIndex] {
+			n[nIndex] = sp[spIndex]
+			spIndex++
+		}
+
+		nIndex++
+	}
+
+	for fpIndex < len(fp) {
+		n[nIndex] = fp[fpIndex]
+
+		fpIndex++
+		nIndex++
+	}
+
+	for spIndex < len(sp) {
+		n[nIndex] = sp[spIndex]
+
+		spIndex++
+		nIndex++
+	}
+
+	return n
+}
+
+// MergeSort !
+func MergeSort(arr []int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+
+	var fp = MergeSort(arr[0 : len(arr)/2])
+	var sp = MergeSort(arr[len(arr)/2:])
+
+	return Merge(fp, sp)
+
+}
+
 func main() {
 	n := []int{1, 39, 2, 9, 7, 54, 11}
 	fmt.Println("BubbleSort:", BubbleSort(n))
@@ -70,4 +121,5 @@ func main() {
 	fmt.Println("RecursiveBubbleSort:", RecursiveBubbleSort(n, len(n)))
 	fmt.Println("InsertionSort", InsertionSort(n))
 	fmt.Println("SelectionSort", SelectionSort(n))
+	fmt.Println("MergeSort", MergeSort(n))
 }
